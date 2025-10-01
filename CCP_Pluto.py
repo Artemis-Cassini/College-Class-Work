0bd4236ef3fd63f73ace6457c264cb12d005444f
Artemis <cara@shoemaker.farm>
2025-09-30 23:29:21 -0400

    Delete CCP_Pluto.py

--- CCP_Pluto.py
+++
@@ -1,7 +0,0 @@
-proportion = 6
-
-print("Is Pluto a planet?")
-
-print("Some people think so, but others don't.")
-print("\tThe Moon's mass is" + str(proportion) + "times Pluto's.")
-print("\tNot much of a planet, is it?")
