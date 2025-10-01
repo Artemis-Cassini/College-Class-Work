84f3748b405a1985a3a5cba3adb0af2da8a2de16
Artemis <cara@shoemaker.farm>
2025-09-30 23:26:36 -0400

    Create CCP_6.1_LAB.py

---
+++ CCP_Chapter_6/CCP_6.1_LAB.py
@@ -0,0 +1,10 @@
+caffeine_mg = float(input())
+
+your_value = caffeine_mg // 2
+print("After 6 hours:", f"{your_value:.2f}")
+
+your_value2 = caffeine_mg = your_value // 2
+print("After 12 hours:", f"{your_value2:.2f}")
+
+your_value3 = your_value2 / 4
+print("After 24 hours:", f"{your_value3:.2f}")
